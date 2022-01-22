5bfc195ea22a9a6a6588050591bc7ee9165427b2
Oanliju <48629684+Oanliju@users.noreply.github.com>
2022-01-22 17:47:07 +0400

    Update script.js

--- pages/script.js
+++ pages/script.js
@@ -10,7 +10,16 @@ class Question {
   }
   let questions = [
     new Question("Parmis ces waifus, laquelle est dans l'anime Sword art online", ["Mai Sakurajima", "Zero Two", "Rem", "Asuna Yuuki"], "Asuna Yuuki"),
-    new Question(" ", [" "," ", " ", " - "], " - ")
+    new Question("Dans quel épisode Zoro rejoint-il Luffy ?", ["Episode 1","Episode 2", "Episode 3", "Episode 4"], "Episode 3"),
+    new Question("Quelle est l'expression favorite de Naruto ?", ["Oh ma gâté !","Le J c'est le S", "Dattebayo", "Skusku !"], "Dattebayo"),
+    new Question("Dans quel épisode voit-on pour la première fois les yeux de Satoru Gojo ?", ["Épisode 6","Épisode 7", "Épisode 8", "Épisode 9"], "Épisode 7"),
+    new Question("Enfin, quelle est la raison de l'alliance entre Luffy et Trafalgar Law ? ", ["Pour vaincre les 4 Empereurs","Pour vaincre la Marine","Pour agrandir leurs équipages", "Pour s'amuser"], "Pour vaincre les 4 Empereurs"),
+    new Question("Par qui Vegeta a-t-il été tué ? ", ["Par Buu","Par Raditz", "Par Cell", "Par Freezer"], "Par Freezer"),
+    new Question("Combien de temps dura la 4ème grande guerre ninja ? (Naruto)", ["10 jours","1 semaine", "4 jours", "2 jours"], "2 jours"),
+    new Question("Qui tua Madara lors de la 4ème grande guerre ninja ? ", ["Kaguya","Zetsu", "Tobi", "Naruto et Sasuke"], "Zetsu"),
+    new Question("Charlotte Katakuri est le combien-tième fils de la famille Charlotte ?", ["1er","2ème", "3ème", "4ème"], "2ème"),
+    new Question("Durant le tournoi en équipes opposant la seconde A et la seconde B, combien de matchs ont lieu ? ", ["3","4", "5", "6"], "5"),
+    new Question("Qui a dit « Avec de l'entrainement, même un raté peut devenir un génie ! »", ["Jiraya","Rock Lee", "Gaï", "Naruto"], "Rock Lee")
   ];
   
   console.log(questions);
